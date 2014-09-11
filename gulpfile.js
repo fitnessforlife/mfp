27bb52a2c700722538bab0c05e947726205f6a75
Andrew Zey <andrew@andrewzey.com>
2014-09-11 10:45:41 -0700

    (initial) add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+
+gulp.task('test', function(){
+  return gulp.src('tests/*.js', {read: false})
+  .pipe(mocha({reporter: 'spec'}));
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./mfp_functions/**', ['test']);
+  gulp.watch('./tests/**', ['test']);
+});
+
+gulp.task('default', ['watch']);
