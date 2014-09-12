2ae38f2f3a5ca72dc04d840edf39ed5333ea4e2a
Andrew Zey <andrew@andrewzey.com>
2014-09-11 20:36:15 -0700

    (feature) add coveralls supports

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var coveralls = require('gulp-coveralls');
 
 gulp.task('test', function(){
   return gulp.src('tests/*.js', {read: false})
-  .pipe(mocha({reporter: 'spec'}));
+  .pipe(mocha({reporter: 'mocha-lcov-reporter'}))
+  .pipe(coveralls());
 });
 
 gulp.task('watch', function(){
