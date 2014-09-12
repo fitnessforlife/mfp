6184f71922474818b7dbfaacaba1241c4a335fa9
Andrew Zey <andrew@andrewzey.com>
2014-09-12 11:31:28 -0700

    (feat) add convertToNum() helpfer function

--- mfp_functions/helper-utils.js
+++ mfp_functions/helper-utils.js
@@ -1,7 +1,19 @@
 'use strict';
 
-var convertToNum = function(){};
-var mfpUrl = function(){};
+var convertToNum = function(string){
+  if (typeof string !== "string") throw new TypeError("Input type must be 'string'");
+  if (string.match(/^[-,0-9]+$/) === null) throw new Error('Input string must only contain numbers and commas');
+
+  return parseInt(string.split(',').join(''));
+};
+
+var mfpUrl = function(userId, date){
+  date = date.slice(0,4) + '-' + date.slice(4,6) + '-' + date.slice(6,8);
+  return 'http://www.myfitnesspal.com/food/diary/' +
+         userId +
+         '?date=' +
+         date;
+};
 
 module.exports = {
   convertToNum: convertToNum,
--- test/helper-utils.spec.js
+++ test/helper-utils.spec.js
@@ -9,6 +9,30 @@ describe('convertToNum', function(){
   it('should be a function', function(){
     (typeof convertToNum).should.equal('function');
   });
+
+  it('should convert strings of numbers to numbers', function(){
+    (convertToNum('5600')).should.equal(5600);
+  });
+
+  it('should convert strings of numbers with commas to numbers', function(){
+    (convertToNum('5,600')).should.equal(5600);
+    (convertToNum('1,000,000')).should.equal(1000000);
+  });
+
+  it('should throw an error for non-string inputs', function(){
+    (function(){ convertToNum(true); }).should.throw(TypeError, "Input type must be 'string'");
+    (function(){ convertToNum([]); }).should.throw(TypeError, "Input type must be 'string'");
+    (function(){ convertToNum({}); }).should.throw(TypeError, "Input type must be 'string'");
+    (function(){ convertToNum(null); }).should.throw(TypeError, "Input type must be 'string'");
+    (function(){ convertToNum(undefined); }).should.throw(TypeError, "Input type must be 'string'");
+    (function(){ convertToNum(1234); }).should.throw(TypeError, "Input type must be 'string'");
+  });
+
+  it('should throw an error if input string contains anything other than numbers and commas', function(){
+    (function(){ convertToNum('abcd'); }).should.throw(Error, 'Input string must only contain numbers and commas');
+    (function(){ convertToNum('1,234abcd'); }).should.throw(Error, 'Input string must only contain numbers and commas');
+  });
+
 });
 
 describe('mfpUrl', function(){
