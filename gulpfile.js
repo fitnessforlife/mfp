53808d2dcac2e998e12d86f289c6dbc5a9d5a992
Andrew Zey <andrew@andrewzey.com>
2014-09-12 10:38:57 -0700

    (refact) cleanup gulp file, improve naming

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,12 @@
+'use strict';
+
 var gulp = require('gulp');
 var jshint = require( 'gulp-jshint' );
 var stylish = require( 'jshint-stylish' );
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
-var coveralls = require('gulp-coveralls');
 
-// gulp.task('blanketTest', function () {
-//   gulp.src(['tests/*.js'], { read: false })
-//   .pipe(mocha({
-//     reporter: 'spec'
-//   }))
-//   .pipe(blanket({
-//     instrument:['tests/*.js'],
-//     captureFile: 'tests/coverage/lcov.html',
-//     reporter: 'html-cov'
-//   }));
-//
-//   gulp.src('tests/coverage/lcov.info')
-//   .pipe(coveralls());
-// });
-
-gulp.task('jshint', function ( cb ) {
+gulp.task('lint', function ( cb ) {
   gulp.src([
     './!(coverage|node_modules)/**/*.js'
   ])
@@ -47,8 +33,8 @@ gulp.task('test', function(cb){
 });
 
 gulp.task('watch', function(){
-  gulp.watch('./mfp_functions/**', ['jshint', 'test']);
-  gulp.watch('./test/**', ['jshint', 'test']);
+  gulp.watch('./mfp_functions/**', ['lint', 'test']);
+  gulp.watch('./test/**', ['lint', 'test']);
 });
 
 gulp.task('default', ['watch']);
