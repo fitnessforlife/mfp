9f054063fa9cb916184e5fc9ff9e3c7bccdc4b01
Andrew Zey <andrew@andrewzey.com>
2014-09-18 14:18:35 -0700

    (feat) add pseudocode for apiStatusCheck()

---
+++ mfp_functions/apiStatusCheck.js
@@ -0,0 +1,21 @@
+'use strict';
+
+//this function checks to see if MyFitnessPal has changed the structure of their data presentation
+//by checking to see if running 'fetchSingleDate' returns the expected data values for a known page
+//Test Page: http://www.myfitnesspal.com/food/diary/npmmfp?date=2014-09-13
+//Expected Values:
+//  Calories: 2078
+//  Carbs: 98
+//  Fat: 119
+//  Protein: 153
+//	Sodium: 3031
+//	Sugar: 14
+
+//check to see if diaryStatusCheck is working correctly (
+  //branch 1: public
+  //branch 2: private
+  //branch 3: invalid user
+
+//check to see if fetchSingleDate is working correctly
+  //all fields
+  //single field
