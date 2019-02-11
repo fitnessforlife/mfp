974dcbdd62a6ff742e37d40d0e7d85576f0eb560
Sam Hinshaw <samuel.hinshaw@gmail.com>
2019-02-10 17:15:18 -0800

    Test authentication for my user account

---
+++ test-auth-user.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// var request = require('request');
+// var fetchSingleDate = require('./mfp_functions/fetchSingleDate');
+var login = require('./mfp_functions/login');
+
+// fetchSingleDate('jetknife', '2019-02-07', ['calories'], function (data) {
+//   console.log(data)
+// });
+
+login('jetknife');
